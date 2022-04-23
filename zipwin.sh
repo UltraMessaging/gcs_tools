10d0cecd65f68eb2e370e76274ed18b631f087df
Steve Ford <sford@geeky-boy.com>
2022-04-23 10:51:21 -0400

    Update zipwin.sh

--- zipwin.sh
+++ zipwin.sh
@@ -3,6 +3,9 @@
 
 rm -rf win64_bin
 mkdir win64_bin
-cp /cygdrive/c/Users/ebuild.WINBUILD/Documents/Visual\ Studio\ 2012/Projects/gcs_tools/x64/Debug/*.exe win64_bin/
+cp ../Documents/Visual\ Studio\ 2012/Projects/gcs_tools/x64/Debug/*.exe win64_bin/
 
 zip -qr win64_bin.zip win64_bin
+
+cd ../Documents/Visual\ Studio\ 2012/Projects/
+tar -czf ../../../gcs_tools/gcs_tools-solution-backup.gz gcs_tools
